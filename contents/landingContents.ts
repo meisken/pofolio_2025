54573d3f1c6c38fded591f95a7f3c20d17de4a17
meisken <meisken999@gmail.com>
2025-05-31 21:47:20 +0800

    add: landing page contents

---
+++ contents/landingContents.ts
@@ -0,0 +1,197 @@
+
+
+export const landingContents = {
+    "en": {
+        hero: {
+            headline: ["modern","aesthetics"],
+            description: "I’m a web developer and designer transform ideas into stunning digital experiences, build modern, visually appealing, clean websites.",
+            backgroundVideo: {
+                videoSrc: "/assets/background.mp4"
+            }
+        },
+        "what-i-do": {
+            header: "What i do",
+            description: "I creates memorable websites with smooth animations and interactive experiences.",
+            "3d-hover-card": {
+                caption: "3D card hover effect",
+                imgSrc: "/assets/code2.png"
+            },
+            "image-parallax": {
+                caption: "image parallax",
+                imgSrc: "/assets/pexels-leeloothefirst-7818239.jpg"
+            },
+            "parallax-scrolling":{
+                caption: "There are many more techniques I can develop, such as parallax scrolling.",
+                description: "Parallax scrolling a trending web design technique   to add depth and visual interest to the design. It creates a dynamic experience that keeps users engaged.",
+                imgSrcs: ["/assets/lennon-cheng-yAeUPmbyS-0-unsplash.jpg","pexels-nicholas-garman-2492577-4107723.jpg","pexels-iriser-1083807.jpg"]
+            }
+        },
+        "full-screen-transition": {
+            header: ["Full screen","transition"],
+            imgSrcs: ["/assets/pexels-cottonbro-8721339.jpg","/assets/pexels-cottonbro-8721318.jpg"]
+        },
+        "projects": {
+            header: "Projects",
+            description: "I worked on these projects as both a developer and designer, writing code to build them while designing in Figma.",
+            projects: [
+                {
+                    videoSrc: "",
+                    description: "Here’s a simple, clean and fast portfolio website I created in a long time ago. There’s some basic design and a bit animation."
+                },
+                {
+                    videoSrc: "",
+                    description: "Here's the project I worked for South China Morning Post: HK. It's a storytelling page with image sequence and a mobile first design"
+                },
+                {
+                    videoSrc: "",
+                    description: "Here’s a historical website with a 3D animation, but presented with image sequence. And 4 page for each hero stories with a lot of scroll animation."
+                },
+                {
+                    videoSrc: "",
+                    description: "Here’s a demo web app that designed for time management with clean, Intuitive user experience, user friendly design "
+                }
+            ],
+        },
+        "services": {
+            header: "Services",
+            description: "I offer a range of services focused on web solutions—whether it's brand building or work automation",
+            services: [
+                {
+                    caption: "Web Design",
+                    description: "I design clean, elegant, and user-friendly websites. interfaces that feel effortless while maintaining strong visual appeal."
+                },
+                {
+                    caption: "Web Development",
+                    description: "I write clean, maintainable code using TypeScript and ESLint. With Next.js or React and Tailwind, I build reusable components for efficient development."
+                },
+                {
+                    caption: "Chrome extension Development",
+                    description: "Sometimes, you may need to perform repetitive tasks on certain websites. I'm here to automate your workflow and save you time."
+                },
+                {
+                    caption: "Hosting & Cloudflare",
+                    description: "I can host your websites on all major hosting providers and set up Cloudflare to enhance speed and security."
+                },
+            ]
+        },
+        "get-in-touch": {
+            header: "Get in touch",
+            contacts: [
+                {
+                    type: "email",
+                    contact: "meiskena999@gmail.com"
+                },
+                {
+                    type: "whatsapp",
+                    contact: "+852 92053876"
+                },
+                {
+                    type: "upwork",
+                    contact: "Ken Tang"
+                }
+            ]
+        },
+        "custom-mouse": {
+            "scroll": "scroll down",
+            "hover": "hover me"
+        },
+        "lets-talk-button": "Let’s Talk"
+    },
+    
+    "zh": {
+        hero: {
+            headline: ["modern","aesthetics"],
+            description: "I’m a web developer and designer transform ideas into stunning digital experiences, build modern, visually appealing, clean websites.",
+            backgroundVideo: {
+                videoSrc: "/assets/background.mp4"
+            }
+        },
+        "what-i-do": {
+            header: "What i do",
+            description: "I creates memorable websites with smooth animations and interactive experiences.",
+            "3d-hover-card": {
+                caption: "3D card hover effect",
+                imgSrc: "/assets/code2.png"
+            },
+            "image-parallax": {
+                caption: "image parallax",
+                imgSrc: "/assets/pexels-leeloothefirst-7818239.jpg"
+            },
+            "parallax-scrolling":{
+                caption: "There are many more techniques I can develop, such as parallax scrolling.",
+                description: "Parallax scrolling a trending web design technique   to add depth and visual interest to the design. It creates a dynamic experience that keeps users engaged.",
+                imgSrcs: ["/assets/lennon-cheng-yAeUPmbyS-0-unsplash.jpg","pexels-nicholas-garman-2492577-4107723.jpg","pexels-iriser-1083807.jpg"]
+            }
+        },
+        "full-screen-transition": {
+            header: ["Full screen","transition"],
+            imgSrcs: ["/assets/pexels-cottonbro-8721339.jpg","/assets/pexels-cottonbro-8721318.jpg"]
+        },
+        "projects": {
+            header: "Projects",
+            description: "I worked on these projects as both a developer and designer, writing code to build them while designing in Figma.",
+            projects: [
+                {
+                    videoSrc: "",
+                    description: "Here’s a simple, clean and fast portfolio website I created in a long time ago. There’s some basic design and a bit animation."
+                },
+                {
+                    videoSrc: "",
+                    description: "Here's the project I worked for South China Morning Post: HK. It's a storytelling page with image sequence and a mobile first design"
+                },
+                {
+                    videoSrc: "",
+                    description: "Here’s a historical website with a 3D animation, but presented with image sequence. And 4 page for each hero stories with a lot of scroll animation."
+                },
+                {
+                    videoSrc: "",
+                    description: "Here’s a demo web app that designed for time management with clean, Intuitive user experience, user friendly design "
+                }
+            ],
+        },
+        "services": {
+            header: "Services",
+            description: "I offer a range of services focused on web solutions—whether it's brand building or work automation",
+            services: [
+                {
+                    caption: "Web Design",
+                    description: "I design clean, elegant, and user-friendly websites. interfaces that feel effortless while maintaining strong visual appeal."
+                },
+                {
+                    caption: "Web Development",
+                    description: "I write clean, maintainable code using TypeScript and ESLint. With Next.js or React and Tailwind, I build reusable components for efficient development."
+                },
+                {
+                    caption: "Chrome extension Development",
+                    description: "Sometimes, you may need to perform repetitive tasks on certain websites. I'm here to automate your workflow and save you time."
+                },
+                {
+                    caption: "Hosting & Cloudflare",
+                    description: "I can host your websites on all major hosting providers and set up Cloudflare to enhance speed and security."
+                },
+            ]
+        },
+        "get-in-touch": {
+            header: "Get in touch",
+            contacts: [
+                {
+                    type: "email",
+                    contact: "meiskena999@gmail.com"
+                },
+                {
+                    type: "whatsapp",
+                    contact: "+852 92053876"
+                },
+                {
+                    type: "upwork",
+                    contact: "Ken Tang"
+                }
+            ]
+        },
+        "custom-mouse": {
+            "scroll": "scroll down",
+            "hover": "hover me"
+        },
+        "lets-talk-button": "Let’s Talk"
+    }
+}
