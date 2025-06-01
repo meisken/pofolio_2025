a6914c1ddd2ac188910e175f7bc1bbc6e05b8bb0
meisken <meisken999@gmail.com>
2025-06-01 19:01:03 +0800

    optimize: lang redirect based on cookies

--- contents/supportedLanguagesData.ts
+++ contents/supportedLanguagesData.ts
@@ -1,3 +1,5 @@
 export type SupportedLanguages = "en" | "zh"
 export const supportedLanguages: SupportedLanguages[] = ["en" , "zh"] as const;
 export const langQueryHeaderName = "x-lang-query";
+export const userLastVisitLanguageCookiesName = "user-lang"
+export const langUrlQueryName = "lang";
--- middleware/languageRedirect.ts
+++ middleware/languageRedirect.ts
@@ -1,49 +1,71 @@
 import { NextRequest, NextResponse } from "next/server";
-import { hasVisitedCookie } from "./hasVisitedCookie";
-import { langQueryHeaderName, supportedLanguages } from "@/contents/supportedLanguagesData";
+import { langQueryHeaderName, langUrlQueryName, SupportedLanguages, supportedLanguages, userLastVisitLanguageCookiesName } from "@/contents/supportedLanguagesData";
+
+
 
 export const languageRedirect =  (request: NextRequest) => {
 
+    const defaultLang: SupportedLanguages = "en";
     const url = request.nextUrl.clone();
-    let response: NextResponse | undefined = undefined;
+    
 
-    const updateLangQuery = () => {
-
+    const userLangCookies = request.cookies.get(userLastVisitLanguageCookiesName);
+    const { nextUrl: { searchParams } } = request;
+    const langQuery = searchParams.get(langUrlQueryName);
+    
+    const getUserLanguagePreferences = () => {
         const userLanguagePreferences = request.headers.get("Accept-Language") ?? "";
         const getTopLanguage = userLanguagePreferences.split(",")[0];
         const matchFistTwoCharacters =  getTopLanguage.match(/^.{2}/);
-        const languageCode = matchFistTwoCharacters !== null ? matchFistTwoCharacters[0] : "en";
-        const isUserLanguageSupported = supportedLanguages.some((language => language === languageCode));
+        const languageCode = matchFistTwoCharacters !== null ? matchFistTwoCharacters[0] : defaultLang;
+        return languageCode;
+    }
 
-        if(isUserLanguageSupported){
-            url.searchParams.set("lang",languageCode);
-        }else{
-            url.searchParams.set("lang","en");
+    const setLangCookies = (response: NextResponse<unknown>, lang: string | null) => {
+        response.cookies.set(userLastVisitLanguageCookiesName, lang ?? defaultLang, { maxAge: 3600 * 24 * 30 });
+    }
+    const setLangHeader = (response: NextResponse<unknown>, lang: string | null) => {
+        response.headers.set(langQueryHeaderName, lang ?? defaultLang);
+    }
+    const getIsSupported = (lang: string | null) => {
+        return supportedLanguages.some((language => language === lang));
+    }
+    const setSearchParams = (lang: string | null) => {
+        url.searchParams.set(langUrlQueryName, lang ?? defaultLang);
+    }
+
+    const updateLangQuery = (_languageCode: string) => {
+        let lang = _languageCode;
+
+        const isUserLanguageSupported = getIsSupported(lang);
+        if(!isUserLanguageSupported){
+            lang = defaultLang;
         }
+
+        setSearchParams(lang);
+        let response: NextResponse | undefined = undefined;
         response = NextResponse.redirect(url);
+
+        setLangCookies(response, lang);
+        setLangHeader(response, lang);
+
+        return response
+    }
+
+    const isLangQuerySupported = getIsSupported(langQuery);
+
+    if(userLangCookies === undefined){
+        const languageCode = getUserLanguagePreferences();
+        return updateLangQuery(languageCode);
+    }
     
+    if(!isLangQuerySupported || userLangCookies.value !== langQuery){
+        return updateLangQuery(userLangCookies.value);
     }
 
-    const hasVisited =  hasVisitedCookie(request, (setCookies) => {
-        //if it have not been visited callback
-        updateLangQuery();
-        if(response){
-            setCookies(response);
-        }
-      
-    });
-
-    const { nextUrl: { searchParams } } = request;
-    const langQuery = searchParams.get("lang");
-    const isLangQuerySupported = supportedLanguages.some((language => language === langQuery));
-    
-    if(!isLangQuerySupported && hasVisited){
-        updateLangQuery();
-    }
-
-    if(response === undefined){
-        response = NextResponse.next()
-    }
-    response.headers.set(langQueryHeaderName,langQuery ?? "en")
-    return response
+    const response = NextResponse.next();
+    setLangHeader(response, userLangCookies.value);
+    return response;
+        
+ 
 }
