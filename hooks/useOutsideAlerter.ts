33c4551877bc3e7c6d8cc5639a39bace9306cad0
meisken <meisken999@gmail.com>
2025-06-01 23:24:50 +0800

    add: click outside listener hook

---
+++ hooks/useOutsideAlerter.ts
@@ -0,0 +1,21 @@
+import { DependencyList, RefObject, useEffect } from "react";
+
+
+export function useOutsideAlerter(ref: RefObject<HTMLElement | null>, callback: () => void = () => {}, deps: DependencyList = []) {
+    useEffect(() => {
+        console.log("refreshed")
+        function handleClickOutside(event: MouseEvent) {
+            if(ref.current && event.target && !ref.current.contains(event.target as Node)){
+                callback()
+            }
+           
+       
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [ref,...deps]);
+}
+
+
