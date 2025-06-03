9299d1c9c8d02f1bbfd36bf675eddca4c8fa0e7c
meisken <meisken999@gmail.com>
2025-06-03 12:35:17 +0800

    update: linear gradient value

--- components/page/landing/sections/hero/widget/BackgroundVideo.tsx
+++ components/page/landing/sections/hero/widget/BackgroundVideo.tsx
@@ -5,10 +5,10 @@ import { FC } from 'react'
 const BackgroundVideo: FC = () => {
     return (
         <div className='absolute inset-0 z-[-1] '>
-            <video className="w-full h-full object-cover" autoPlay muted loop playsInline >
+            <video className="w-full h-full object-cover" autoPlay muted loop playsInline preload='auto'>
                 <source  src='/assets/background.mp4' type='video/mp4' />
             </video>
-            <div className='absolute bottom-[-1px] left-0 right-0 h-[23.6%]' style={{backgroundImage:"linear-gradient(to top, rgba(0,0,0,1), rgba(0,0,0,0))"}}></div>
+            <div className='absolute bottom-[-1px] left-0 right-0 h-[15%]' style={{backgroundImage:"linear-gradient(to top, rgba(0,0,0,1) 0%, rgba(0,0,0,0.6) 30%, rgba(0,0,0,0) 100%) "}}></div>
         </div>
     );
 };
