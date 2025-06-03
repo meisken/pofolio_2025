74f35831d161084c6650e40a27c0fa5beea21845
meisken <meisken999@gmail.com>
2025-06-03 15:09:50 +0800

    replace: hard coding

--- components/page/landing/sections/hero/widget/BackgroundVideo.tsx
+++ components/page/landing/sections/hero/widget/BackgroundVideo.tsx
@@ -1,12 +1,15 @@
+import { landingContents } from '@/contents/landingContents';
+import { useCurrentLanguages } from '@/contents/languageSupportHooks';
 import { FC } from 'react'
 
 
 
 const BackgroundVideo: FC = () => {
+    const lang = useCurrentLanguages();
     return (
         <div className='absolute inset-0 z-[-1] '>
             <video className="w-full h-full object-cover" autoPlay muted loop playsInline preload='auto'>
-                <source  src='/assets/background.mp4' type='video/mp4' />
+                <source  src={landingContents[lang].hero.backgroundVideo.videoSrc} type='video/mp4' />
             </video>
             <div className='absolute bottom-[-1px] left-0 right-0 h-[15%]' style={{backgroundImage:"linear-gradient(to top, rgba(0,0,0,1) 0%, rgba(0,0,0,0.6) 30%, rgba(0,0,0,0) 100%) "}}></div>
         </div>
