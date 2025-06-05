4c540be0f6b09e5c6908df302ac91b302b0a0dd3
meisken <meisken999@gmail.com>
2025-06-05 16:53:11 +0800

    update: disable splitText

--- lib/gsap/GsapPluginRegisterWrapper.tsx
+++ lib/gsap/GsapPluginRegisterWrapper.tsx
@@ -2,13 +2,13 @@
 import { FC, ReactNode } from 'react'
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { SplitText } from 'gsap/SplitText';
+//import { SplitText } from 'gsap/SplitText';
 
 interface Props{
     children: ReactNode
 }
 gsap.registerPlugin(ScrollTrigger);
-gsap.registerPlugin(SplitText);
+// gsap.registerPlugin(SplitText);
 
 const GsapPluginRegisterWrapper: FC<Props> = ({children}) => {
     return (
