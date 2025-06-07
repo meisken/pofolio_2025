50c7602a67c299efc1bf18796faa84cf00d2a52e
meisken <meisken999@gmail.com>
2025-06-07 23:38:00 +0800

    fix: window is undefined

--- hooks/useMediaQuery.ts
+++ hooks/useMediaQuery.ts
@@ -5,24 +5,30 @@ import { useEffect, useState } from "react";
 
 
 function useMediaQuery(mediaQuery: string): boolean{
-    
-    const [isMatch, setIsMatch] = useState(window.matchMedia(mediaQuery).matches);
-  
-    
-    useEffect(() => {
-    
-        const _mediaQueryList = window.matchMedia(mediaQuery);
-        setIsMatch( _mediaQueryList .matches);
-        _mediaQueryList.onchange = (e) => {
-            setIsMatch(e.matches);
-        }
-        return () => {
-            _mediaQueryList.onchange = null
-        }
-    
-    },[mediaQuery]);
-    return isMatch
  
+        const [isMatch, setIsMatch] = useState(false);
+    
+        
+        useEffect(() => {
+            if(typeof window !== "undefined"){
+       
+
+                const _mediaQueryList = window.matchMedia(mediaQuery);
+                setIsMatch( _mediaQueryList.matches);
+                
+                _mediaQueryList.onchange = (e) => {
+                    setIsMatch(e.matches);
+                }
+                return () => {
+                    _mediaQueryList.onchange = null
+                }
+            }
+        
+        
+        },[mediaQuery]);
+
+        return isMatch
+    
 }
 
 
