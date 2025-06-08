2370a2acefdf4274b54be9ebf2406dcba99c7bd5
meisken <meisken999@gmail.com>
2025-06-09 01:49:10 +0800

    update: hero background scroll parallax

--- components/page/landing/sections/hero/widget/BackgroundVideo.tsx
+++ components/page/landing/sections/hero/widget/BackgroundVideo.tsx
@@ -3,30 +3,57 @@ import { landingContents } from '@/contents/landingContents';
 import { useCurrentLanguages } from '@/contents/languageSupportHooks';
 import { FC } from 'react'
 import ZoomAndFadeLandingAnimation from '@/components/utilities/animations/landing/ZoomAndFadeLandingAnimation';
+import CustomScrollAnimation from '@/components/utilities/animations/scroll/CustomScrollAnimation';
+import useMediaQuery from '@/hooks/useMediaQuery';
 
 
 const BackgroundVideo: FC = () => {
     const lang = useCurrentLanguages();
+    const portraitScreen = useMediaQuery("(orientation: portrait)");
     return (
         <div className='absolute inset-0 z-[-1] portrait:relative portrait:w-full block overflow-clip'>
-            <ZoomAndFadeLandingAnimation 
+            <CustomScrollAnimation
                 className='w-full h-full'
-                duration={1}
-                delay={0.7}
-            >
-                <video 
-                    className="w-full h-full object-cover remoteVideo" 
-                    playsInline  
-                    muted 
-                    autoPlay  
-                    loop
-                    src={landingContents[lang].hero.backgroundVideo.videoSrc}
-                >
-                 
-                </video>
-            </ZoomAndFadeLandingAnimation>
+                styleFrom={{
+                    
+                    transform: "scale(1) translateY(0%)"
 
-            <div className='absolute bottom-[-1%] left-0 right-0 h-[5%]' style={{backgroundImage:"linear-gradient(to top, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 100%) "}}></div>
+                }}
+                styleTo={{
+                    
+                    transform:   portraitScreen ? "" : "scale(1.1) translateY(33%)"
+                }}
+                ease={"none"}
+                trigger={"self"}
+        
+                scrollTriggerVars={{
+                    start: () => "top top",
+                    end: () => `bottom top`,
+                    scrub: 0,
+                    
+                }}
+
+            >
+                <ZoomAndFadeLandingAnimation 
+                    className='w-full h-full'
+                    duration={1}
+                    delay={0.7}
+                >
+                
+                        <video 
+                            className="w-full h-full object-cover remoteVideo" 
+                            playsInline  
+                            muted 
+                            autoPlay  
+                            loop
+                            src={landingContents[lang].hero.backgroundVideo.videoSrc}
+                        >
+                        
+                        </video>
+                    
+                </ZoomAndFadeLandingAnimation>
+            </CustomScrollAnimation>
+            <div className='absolute bottom-0 left-0 right-0 h-[5%] scale-y-150' style={{backgroundImage:"linear-gradient(to top, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 100%) "}}></div>
         </div>
     );
 };
