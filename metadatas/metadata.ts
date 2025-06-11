9fd1e98a8ea0dc8f5bc0592c7bd648cfa0edde66
meisken <meisken999@gmail.com>
2025-06-11 16:34:33 +0800

    add: metadata

---
+++ metadatas/metadata.ts
@@ -0,0 +1,46 @@
+import { Metadata } from "next";
+
+export const metadataTags: Metadata = {
+    title: "Ken Tang's portfolio 2025",
+    description: "I am a web developer and designer who crafts dynamic websites that deliver interactive experiences far beyond mere static text and images.",
+    keywords: ["web developer","web designer","web development","parallax","scroll animation","animation","freelance","visual appeal","minimalist","landing page","portfolio", "interactive experiences", "smooth animations"],
+    publisher: "Ken Tang",
+    authors: [{name: "Ken Tang",url: "https://www.upwork.com/freelancers/~01698d1c0db877b32d"}],
+    robots:"index, follow",
+    icons: {
+        icon: "/assets/icon.png",
+        apple: "/assets/icon.png"
+    },
+    formatDetection: {
+        email: false,
+        address: false,
+        telephone: false,
+    },
+    alternates: {
+        canonical: "https://www.meisken.dev",
+        languages: {
+            en: "en-US",
+            zh: "zh-HK"
+        }
+    },
+    category: 'portfolio',
+    openGraph: {
+        siteName: "Ken Tang's portfolio 2025",
+        type: "website",
+        title: "Ken Tang's portfolio 2025",
+        description: 'I am a web developer and designer who crafts dynamic websites that deliver interactive experiences far beyond mere static text and images.',
+        images: "/assets/thumbnail.png",
+        url: "https://www.meisken.dev",
+        locale: "Hong Kong",
+        emails: "meiskena999@gmail.com",
+        phoneNumbers: "+85292053876",
+    },
+    twitter: {
+        site: "@Ken Tang",
+        creator: "@Ken Tang",
+        card: 'summary_large_image',
+        title: "Ken Tang's portfolio 2025",
+        description: "I am a web developer and designer who crafts dynamic websites that deliver interactive experiences far beyond mere static text and images.",
+        images: "/assets/thumbnail.png",
+    }
+};
