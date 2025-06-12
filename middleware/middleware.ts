8fbf59eae20332aa9af1cf51e1ddf9a8e473c990
meisken <meisken999@gmail.com>
2025-06-12 23:32:11 +0800

    update: disable server side languageRedirect

--- middleware/middleware.ts
+++ middleware/middleware.ts
@@ -1,16 +1,16 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { languageRedirect } from './middleware/languageRedirect';
+//import { languageRedirect } from './middleware/languageRedirect';
 
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    const { pathname } = request.nextUrl;
-    if (pathname.startsWith('/_next') || pathname.startsWith('/api')) {
-        return NextResponse.next();
-    }
-    
-    return languageRedirect(request)
+    // const { pathname } = request.nextUrl;
+    // if (pathname.startsWith('/_next') || pathname.startsWith('/api')) {
+    //     return NextResponse.next();
+    // }
+    NextResponse.next();
+    // return languageRedirect(request)
 }
  
 // See "Matching Paths" below to learn more
