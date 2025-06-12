7d2cfedcad9fda4f88c243043380cfae317864f9
meisken <meisken999@gmail.com>
2025-06-12 12:22:02 +0800

    add: chinese contents

--- contents/AboutContents.ts
+++ contents/AboutContents.ts
@@ -23,8 +23,8 @@ export const aboutContents: AboutContents = {
     "zh": {
         hero: {
             headline: "Independent web developer based in hong kong",
-            subheading: "I’m committed to developing websites that deliver memorable digital experiences through clean, reusable code.",
-            description: "As a web developer, I'm obsessed with creative animations, and I'm here to share my passion with you. Let's transform websites into dynamic experiences—far beyond just static text and images.",
+            subheading: "我致力於開發令人難忘數字體驗的網站. 井且使用簡潔且可重複再用的代碼, 減低日後更新的成本.",
+            description: "作為一名網頁開發者, 我熱愛於很有創意動畫, 並願與您分享我的熱情, 讓我們把網頁轉變為充滿動態體驗的空間, 不再只是靜態的文字和圖像。",
             imgSrc: "/assets/pexels-soheilkmp-27413969.jpg"
         },
     }
--- contents/landingContents.ts
+++ contents/landingContents.ts
@@ -112,10 +112,6 @@ export type LandingContents = Record<SupportedLanguages,{
             ],
             "hand-image-src": string
         },
-        "custom-mouse": {
-            "scroll": string,
-            "hover": string
-        },
         "lets-talk-button": {
             name: string,
             link: string
@@ -234,10 +230,6 @@ export const landingContents: LandingContents = {
             ],
             "hand-image-src": "/assets/hand.PNG"
         },
-        "custom-mouse": {
-            "scroll": "scroll down",
-            "hover": "hover me"
-        },
         "lets-talk-button": {
             name: "Let’s Talk",
             link: "https://www.upwork.com/freelancers/~01698d1c0db877b32d"
@@ -246,9 +238,9 @@ export const landingContents: LandingContents = {
     
     "zh": {
         hero: {
-            headline: ["modern","讓創意飛揚","讓創新綻放"],
-            headlineTypingSpeed: 280,
-            description: "I’m a web developer and designer transform ideas into stunning digital experiences, build modern, visually appealing, clean websites.",
+            headline: ["就在這","讓創意飛揚","讓創新綻放"],
+            headlineTypingSpeed: 200,
+            description: "我是KEN, 一名網頁開發者和設計師, 專門製作現代化/簡潔/有質感的動態網頁。",
             backgroundVideo: {
                 videoSrc: "/assets/background.mp4"
             },
@@ -258,87 +250,87 @@ export const landingContents: LandingContents = {
             }
         },
         "what-i-do": {
-            heading: "What i do",
-            description: "I creates memorable websites with smooth animations and interactive experiences.",
+            heading: "我做什麼的",
+            description: "我會用不同的動畫/互動元素, 來打造讓人難忘的體驗.",
             "3d-hover-card": {
-                caption: "3D card hover effect",
+                caption: "立體滑鼠懸停效果",
                 imgSrc: "/assets/code2.png"
             },
             "image-parallax": {
-                caption: "image hover parallax",
+                caption: "視差滑鼠懸停效果",
                 imgSrc: "/assets/pexels-leeloothefirst-7818239.jpg"
             },
             "parallax-scrolling":{
-                caption: "There are many more techniques I can develop, such as parallax scrolling.",
-                description: "Parallax scrolling a trending web design technique   to add depth and visual interest to the design. It creates a dynamic experience that keeps users engaged.",
-                imgSrcs: ["/assets/lennon-cheng-yAeUPmbyS-0-unsplash.jpg","/assets/pexels-nicholas-garman-2492577-4107723.jpg","/assets/pexels-iriser-1083807.jpg"],
-                imgsCaptions: ["slow", "fast","speedy"]
+                caption: "還有更多的動態效果,在等你發掘.",
+                description: "視差是一種流行的網頁技術, 可為設計增添深度和視覺趣味. 它創造出更動態體驗, 不再是平平無奇的文字和照片, 增加使用者留在網頁的時間.",
+                imgSrcs: ["/assets/lennon-cheng-yAeUPmbyS-0-unsplash.jpg","/assets/valentin-beauvais-yVUQlyRlJSw-unsplash.jpg","/assets/joel-fulgencio-01fAtHwYqo0-unsplash.jpg"],
+                imgsCaptions: ["慢速", "極快","快速"]
             }
         },
         "full-screen-transition": {
-            heading: ["Full screen","transition"],
+            heading: ["全螢幕","過渡動畫"],
             imgSrcs: ["/assets/pexels-cottonbro-8721339.jpg","/assets/pexels-cottonbro-8721318.jpg"]
         },
         "projects": {
-            heading: "Projects",
-            description: "I worked on these projects as both a developer and designer, writing code to build them while designing in Figma.",
+            heading: "其他作品",
+            description: "這些是我過往的一些作品, 我在Figma設計, 然後寫代碼實現",
             projects: [
                 {
-                    caption: "HALL OF FAME",
-                    videoSrc: "/assets/hall_of_fame.mp4",
-                    description: "Here’s a historical website with a 3D animation. And 4 unique pages for each hero stories with a lot of scroll animations.",
+                    caption: "歷史人物殿堂",
+                    videoSrc: "/assets/projectThumbnail/hall.jpg",
+                    description: "這是一個帶有 3D 動畫效果的歷史網站, 同時為每位人物提供了各具特色的頁面, 頁面上有不同的滾動動畫效果.",
                     projectLink: "https://hall.meisken.dev/"
                 },
                 {
-                    caption: "Portfolio V1",
-                    videoSrc: "/assets/hall_of_fame.mp4",
-                    description: "Here’s a simple, clean and fast portfolio website I created in a long time ago. There’s some basic design and a bit animation.",
+                    caption: "作品集 V1",
+                    videoSrc: "/assets/projectThumbnail/old_portfolio.jpg",
+                    description: "這是我很久以前創建的一個簡單, 簡潔且快速的作品集網站, 裏面有一些基本設計和一點動畫效果.",
                     projectLink: "https://portfolio.meisken.dev/"
                 },
                 {
                     caption: "How moons slow retreat earth",
-                    videoSrc: "/assets/hall_of_fame.mp4",
-                    description: "Here's the project I worked for South China Morning Post: HK, a storytelling page with image sequence and a mobile first design.",
+                    videoSrc: "/assets/projectThumbnail/moon.jpg",
+                    description: "這是我為《南華早報：香港》製作的一個項目, 一個採用圖像序列動畫和手機優先設計的頁面. ",
                     projectLink: "https://multimedia.scmp.com/native/infographics/article/3131059/how-moons-slow-retreat-earth/"
                 },
                 {
-                    caption: "Todo calendar web app",
-                    videoSrc: "/assets/hall_of_fame.mp4",
-                    description: "Here’s a demo web app that designed for time management with clean, Intuitive user experience, user friendly design.",
+                    caption: "待辦事項和日歷應用",
+                    videoSrc: "/assets/projectThumbnail/todo.jpg",
+                    description: "這是一個為時間管理而設計的網頁應用 demo, 擁有簡潔/直觀的用户體驗和用户友好的設計.",
                     projectLink: "https://todo.meisken.dev/"
                 },
                 {
-                    caption: "chinese text converter",
-                    videoSrc: "/assets/hall_of_fame.mp4",
-                    description: "It provides functions for Traditional/Simplified Chinese conversion, image-to-text conversion, capturing screenshots.",
+                    caption: "文字轉換工具",
+                    videoSrc: "/assets/projectThumbnail/tools.jpg",
+                    description: "這是免費的chrome插件, 提供繁簡轉換/圖片轉文字/已選擇部份截圖/速成碼轉換的功能,可在跳出視窗/右鍵選單中使用",
                     projectLink: "https://chromewebstore.google.com/detail/%E6%96%87%E5%AD%97%E8%BD%89%E6%8F%9B%E5%B7%A5%E5%85%B7/efdjngbfhfhmambhgcfbnmpggjgfalpg?authuser=5&hl=zh-TW"
                 }
             ],
         },
         "services": {
-            heading: "Services",
-            description: "I offer a range of services focused on web solutions—whether it's brand building or work automation",
+            heading: "服務",
+            description: "我提供一系列服務, 主要以在網頁實現為主, 無論是品牌建設還是工作自動化.",
             services: [
                 {
-                    caption: "Web Design",
-                    description: "I design clean, elegant, and user-friendly websites. interfaces that feel effortless while maintaining strong visual appeal."
+                    caption: "網頁設計",
+                    description: "我設計簡潔、優雅且用户友好的網站和界面, 這些界面操作起來流暢, 同時具有視覺吸引力。"
                 },
                 {
-                    caption: "Web Development",
-                    description: "I write clean, maintainable code using TypeScript and ESLint. With Next.js or React and Tailwind, I build reusable components for efficient development."
+                    caption: "網頁開發",
+                    description: "我使用 TypeScript 和 ESLint 編寫整潔且易於維護的代碼, 利用 Next.js 或 React 結合 Tailwind, 我構建可複用組件以實現高效開發."
                 },
                 {
-                    caption: "Chrome extension Development",
-                    description: "Sometimes, you may need to perform repetitive tasks on certain websites. I'm here to automate your workflow and save you time."
+                    caption: "Chrome擴充功能開發",
+                    description: "有時候, 你可能需要在某些網站上執行重複任務, 我在這裏幫助你自動化工作流程, 為你節省時間."
                 },
                 {
-                    caption: "Hosting & Cloudflare",
-                    description: "I can host your websites on all major hosting providers and set up Cloudflare to enhance speed and security."
+                    caption: "網頁託管和 Cloudflare",
+                    description: "我可以在所有主流的託管服務提供商上託管您的網站, 並設置 Cloudflare 來提升速度和安全性."
                 },
             ]
         },
         "get-in-touch": {
-            heading: "Get in touch",
+            heading: "聯絡方法",
             contacts: [
                 {
                     type: "email",
@@ -351,14 +343,9 @@ export const landingContents: LandingContents = {
                 {
                     type: "upwork",
                     contact: "Ken Tang"
-                },
-                
+                }
             ],
             "hand-image-src": "/assets/hand.PNG"
-        },
-        "custom-mouse": {
-            "scroll": "scroll down",
-            "hover": "hover me"
         },
         "lets-talk-button": {
             name: "Let’s Talk",
